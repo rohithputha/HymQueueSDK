217fd0588bf3fdc8086aed8aaef73808f85971cf
Rohith <rohith@Rohiths-MacBook-Air.local>
2023-08-28 23:04:36 +0530

    added get response part

--- src/main/java/org/hitro/service/SocketFactory.java
+++ src/main/java/org/hitro/service/SocketFactory.java
@@ -1,9 +1,12 @@
 package org.hitro.service;
 
 import lombok.Getter;
+import org.hitro.binaryprotocol.publicinterfaces.IBinaryProtocol;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Arrays;
 
 @Getter
 public class SocketFactory {
@@ -11,7 +14,14 @@ public class SocketFactory {
 
     private static Socket getConnectedSocket(){
         try {
-            return new Socket("localhost", 3456);
+//            String url = "hym-queue-ws-ma6te.ondigitalocean.app";
+//            InetAddress[] addresses = InetAddress.getAllByName(url);
+//
+//            for (InetAddress address : addresses) {
+//                System.out.println("Resolved IP Address: " + address.getHostAddress());
+//            }
+
+            return new Socket("159.89.197.174", 3456);
         } catch (IOException e) {
             return null;
         }
@@ -23,6 +33,12 @@ public class SocketFactory {
             synchronized (SocketFactory.class){
                if(socket==null || socket.isClosed()){
                    socket =  getConnectedSocket();
+                   try {
+                       DataInterface.getDataInterface().getDataSender().sendData(Arrays.asList("register_socket","sd1324234","localhost"));
+                       //socket.getOutputStream().write(IBinaryProtocol.getInstance().encode(Arrays.asList("register_socket","sd1324234","localhost")));
+                   } catch (InterruptedException e) {
+                       throw new RuntimeException(e);
+                   }
                }
             }
         }
--- src/main/java/org/hitro/service/sync/SyncHymQueue.java
+++ src/main/java/org/hitro/service/sync/SyncHymQueue.java
@@ -4,22 +4,44 @@ import org.hitro.config.HymQueueSdkConfiguration;
 import org.hitro.sdk.publicinterfaces.HymQueue;
 import org.hitro.sdk.publicinterfaces.Subscriber;
 import org.hitro.service.DataInterface;
+import org.hitro.service.SocketFactory;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class SyncHymQueue implements HymQueue {
     private final HymQueueSdkConfiguration hymQueueSdkConfiguration;
-    private SyncHymQueue(HymQueueSdkConfiguration hymQueueSdkConfiguration){
+
+    private final ResposeHolder resposeHolder;
+    private SyncHymQueue(HymQueueSdkConfiguration hymQueueSdkConfiguration, ResposeHolder resposeHolder){
         this.hymQueueSdkConfiguration = hymQueueSdkConfiguration;
+        this.resposeHolder = resposeHolder;
+    }
+
+    private <T> List<T> getResponse(String uuid) throws InterruptedException {
+        List<T> resp = null;
+        int count = 0;
+        do{
+            resp = this.resposeHolder.getResponse(uuid);
+            if(resp!=null || count>100) return resp;
+            count++;
+            Thread.sleep(100);
+        }
+        while (resp == null);
+        return null;
     }
 
     @Override
     public boolean createChannel() {
-        List<String> command = Arrays.asList("create_channel","sdk123","testChannel1","pubsub");
-        try {
-            DataInterface.getDataInterface().getDataSender().sendData(command);
-            System.out.println("data sent...");
 
+        try {
+            String uuid = getUUID();
+            List<String> command = Arrays.asList("create_channel",uuid,hymQueueSdkConfiguration.getChannelName(),hymQueueSdkConfiguration.getChannelType());
+            DataInterface.getDataInterface().getDataSender().sendData(command);
+            String resp = (String) getResponse(uuid).get(1);
+            if((resp).equals("SUCCESS")) {
+                return true;
+            }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -28,41 +50,52 @@ public class SyncHymQueue implements HymQueue {
 
     @Override
     public <T> boolean addData(T data) {
-        List<Object> command = Arrays.asList("add_data","sdk1234",(T)data,"testChannel1","pubsub");
+        String uuid = getUUID();
+        List<Object> command = Arrays.asList("add_data",uuid,(T)data,hymQueueSdkConfiguration.getChannelName(),hymQueueSdkConfiguration.getChannelType());
         try{
             DataInterface.getDataInterface().getDataSender().sendData(command);
-//            System.out.println(dataReceiver.getData());
+            String resp = (String) getResponse(uuid).get(1);
+            if((resp).equals("SUCCESS")) {
+                return true;
+            }
         }
         catch (Exception e){
             throw new RuntimeException(e);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean addSubscriber(Subscriber subscriber) throws InterruptedException {
-        if(this.hymQueueSdkConfiguration.getChannelType().equals("pubsub")){
-            SubscriberNotificationReceiver.getInstance().addSubscriberCallback("testChannel1",subscriber);
-            DataInterface.getDataInterface().getDataSender().sendData(Arrays.asList("add_subscriber","sdk123456","subid123","testChannel1","localhost","3456"));
         }
         return false;
     }
 
     @Override
-    public <T> T getData() {
-        try {
-            DataInterface.getDataInterface().getDataSender().sendData(Arrays.asList("get_data","sdk12345","testChannel1"));
+    public boolean addSubscriber(Subscriber subscriber) throws InterruptedException {
+        if(this.hymQueueSdkConfiguration.getChannelType().equals("pubsub")){
+            SubscriberNotificationReceiver.getInstance().addSubscriberCallback(hymQueueSdkConfiguration.getChannelName(), subscriber);
+            String uuid = getUUID();
+            DataInterface.getDataInterface().getDataSender().sendData(Arrays.asList("add_subscriber",uuid,"localhost"+hymQueueSdkConfiguration.getChannelName(),hymQueueSdkConfiguration.getChannelName(),"localhost","3456"));
+            String resp = (String) getResponse(uuid).get(1);
+            if((resp).equals("SUCCESS")) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    @Override
+    public <T> List<T> getData() {
+        try {
+            String uuid = getUUID();
+            DataInterface.getDataInterface().getDataSender().sendData(Arrays.asList("get_data",uuid,hymQueueSdkConfiguration.getChannelName()));
+            return getResponse(uuid);
         }
         catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 
     public static SyncHymQueue getInstance(HymQueueSdkConfiguration hymQueueSdkConfiguration){
         synchronized (SyncHymQueue.class){
-            return new SyncHymQueue(hymQueueSdkConfiguration);
+            SocketFactory.getSocket();
+            ResposeHolder resposeHolder1 = ResposeHolder.getInstance();
+            return new SyncHymQueue(hymQueueSdkConfiguration,resposeHolder1);
         }
     }
 }
